0f4645332c2a031c7a247e234c3a63f44989a9f1
Karl Stenerud <kstenerud@gmail.com>
2024-10-28 16:22:23 +0100

    init

---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Karl Stenerud on 28.10.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
+void DoSomething(void);
---
+++ MyFramework/MySourceCode.m
@@ -0,0 +1,12 @@
+//
+//  MySourceCode.m
+//  MyFramework
+//
+//  Created by Karl Stenerud on 28.10.24.
+//
+
+#import <Foundation/Foundation.h>
+
+void DoSomething(void) {
+    NSLog(@"Doing something");
+}
